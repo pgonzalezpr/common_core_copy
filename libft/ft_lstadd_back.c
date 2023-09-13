245a580cc36c9e16032a8b4434ce44a7f50924ba
Pedro Javier Gonzalez Prado <pedro-go@car6s6.42barcelona.com>
2023-09-13 14:51:13 +0200

    fixed bonuses

--- libft/ft_lstadd_back.c
+++ libft/ft_lstadd_back.c
@@ -1,21 +1,24 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_lstlast.c                                       :+:      :+:    :+:   */
+/*   ft_lstadd_back.c                                   :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pedro-go <pedro-go@student.42barcel>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/09/13 12:11:04 by pedro-go          #+#    #+#             */
-/*   Updated: 2023/09/13 14:43:49 by pedro-go         ###   ########.fr       */
+/*   Created: 2023/09/13 14:44:13 by pedro-go          #+#    #+#             */
+/*   Updated: 2023/09/13 14:50:28 by pedro-go         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+#include "libft.h"
 
-t_list	*ft_lstlast(t_list *lst)
+void	ft_lstadd_back(t_list **lst, t_list *new)
 {
 	t_list	*current;
 
-	current = lst;
-	while (current->next)
-		current = current->next;
-	return (current);
+	current = *lst;
+	if (!current)
+		ft_lstadd_front(lst, new);
+	else
+		current = ft_lstlast(current);
+	current->next = new;
 }
