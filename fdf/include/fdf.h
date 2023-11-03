c213473aacc1425cc2dc211008769573b42fadef
pgonzalezpr <pgonzalezpr1@gmail.com>
2023-11-03 10:45:54 +0100

    creating fdf. Minilib left to add to Makefile

---
+++ fdf/include/fdf.h
@@ -0,0 +1,7 @@
+#ifndef FDF_H
+# define FDF_H
+
+# include "libft.h"
+# include <math.h>
+
+#endif
---
+++ fdf/srcs/main.c
@@ -0,0 +1,6 @@
+# include "../include/fdf.h"
+
+int main()
+{
+    ft_printf("Hello %s\n", "fdf!");
+}
