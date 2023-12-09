1e1e8ede2e47472de28e719d9f5c886432b6c4a2
Pedro Javier Gonzalez Prado <pedro-go@car12s2.42barcelona.com>
2023-12-09 18:06:17 +0100

    philosophers 9/12/23

--- philosophers/philo/srcs/input.c
+++ philosophers/philo/srcs/input.c
@@ -12,18 +12,6 @@
 
 #include "../include/philo.h"
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
-
-	len = 0;
-	while (*s++)
-	{
-		len++;
-	}
-	return (len);
-}
-
 int	ft_isdigit(int c)
 {
 	if ((c >= '0' && c <= '9'))
@@ -45,21 +33,6 @@ int	is_numeric(char *str)
 	return (1);
 }
 
-int	check_max(char *str)
-{
-	int	ans;
-
-	while (*str == '0')
-		str++;
-	if (*str == '\0')
-		return (1);
-	if (ft_strlen(str) == ft_strlen(MAX_TIME_MS))
-		ans = (ft_strncmp(MAX_TIME_MS, str, ft_strlen(MAX_TIME_MS)) == 0);
-	else
-		ans = (ft_strlen(MAX_TIME_MS) > ft_strlen(str));
-	return (ans);
-}
-
 int	check_input(int argc, char **argv)
 {
 	int	i;
@@ -71,7 +44,7 @@ int	check_input(int argc, char **argv)
 	{
 		if (!is_numeric(argv[i]))
 			return (0);
-		if (i > 1 && i < 5 && !check_max(argv[i]))
+		if (i == 1 && ft_atoi(argv[i]) < 1)
 			return (0);
 		i++;
 	}
