385f7224a94671df248434c7afe5438964953345
Pedro Javier Gonzalez Prado <pedro-go@cbr12s1.42barcelona.com>
2024-04-13 18:29:37 +0200

    main

--- philosophers/philo_bonus/srcs/main_bonus.c
+++
@@ -1,6 +0,0 @@
-#include "../include/philo_bonus.h"
-
-int	main(void)
-{
-	printf("Hello, this is philo bonus!\n");
-}
--- philosophers/philo_bonus/srcs/main_bonus.d
+++
@@ -1,1 +0,0 @@
-srcs/main_bonus.o: srcs/main_bonus.c srcs/../include/philo_bonus.h
