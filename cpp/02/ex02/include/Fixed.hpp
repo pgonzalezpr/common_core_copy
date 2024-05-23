b6c9a0cf4cbaef66df57e8651b0d605f09b98b38
pgonzalezpr <pgonzalezpr1@gmail.com>
2024-05-23 22:01:41 +0200

    cpp02 ex02

---
+++ cpp/02/ex02/include/Fixed.hpp
@@ -0,0 +1,52 @@
+#ifndef FIXED_HPP
+#define FIXED_HPP
+
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+class Fixed
+{
+private:
+    static const int    _fractionalBits;
+    int                 _value;
+public:
+    static Fixed& min(Fixed& first, Fixed& second);
+    static Fixed& max(Fixed& first, Fixed& second);
+    static const Fixed& min(const Fixed& first, const Fixed& second);
+    static const Fixed& max(const Fixed& first, const Fixed& second);
+
+    Fixed();
+    Fixed(const Fixed& other);
+    Fixed(const int value);
+    Fixed(const float fValue);
+    ~Fixed();
+
+    Fixed&  operator=(const Fixed& other);
+    bool    operator==(const Fixed& rhs) const;
+    bool    operator<(const Fixed& rhs) const;
+    bool    operator>(const Fixed& rhs) const; 
+    bool    operator>=(const Fixed& rhs) const;
+    bool    operator<=(const Fixed& rhs) const;
+    bool    operator!=(const Fixed& rhs) const;
+
+    Fixed   operator+(const Fixed& rhs) const;
+    Fixed   operator-(const Fixed& rhs) const;
+    Fixed   operator*(const Fixed& rhs) const;
+    Fixed   operator/(const Fixed& rhs) const;
+
+    Fixed&  operator++();
+    Fixed   operator++(int);
+    Fixed&  operator--();
+    Fixed   operator--(int);
+
+    int     getRawBits() const;
+    void    setRawBits(int const raw);
+    float   toFloat() const;
+    int     toInt() const;
+};
+
+ostream& operator<<(ostream& os, const Fixed& obj);
+
+#endif
