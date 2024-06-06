564dfb5eae5967395fa65e72505cab1bb835575c
pgonzalezpr <pgonzalezpr1@gmail.com>
2024-06-06 22:57:21 +0200

    fixed wrongAnimal class

--- cpp/04/ex00/include/WrongAnimal.hpp
+++ cpp/04/ex00/include/WrongAnimal.hpp
@@ -9,7 +9,7 @@ public:
     WrongAnimal();
     WrongAnimal(const WrongAnimal& other);
     WrongAnimal& operator=(const WrongAnimal& other);
-    ~WrongAnimal();
+    virtual ~WrongAnimal();
     void makeSound() const;
     std::string getType() const;
     void setType(std::string& type);
