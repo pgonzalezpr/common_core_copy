edc693a04691c17fbaedf9e707614294c2c20855
Pedro Javier Gonzalez Prado <pedro-go@cbr6s3.42barcelona.com>
2024-06-20 15:19:48 +0200

    cpp08

--- cpp/08/ex02/srcs/main.cpp
+++ cpp/08/ex02/srcs/main.cpp
@@ -1,6 +1,11 @@
 #include "MutantStack.hpp"
+#include <list>
 
 int main() {
+
+    std::cout << "Test1: Subject" << std::endl;
+    std::cout << std::endl;
+
     MutantStack<int> mstack;
     mstack.push(5);
     mstack.push(17);
@@ -11,7 +16,6 @@ int main() {
     mstack.push(3);
     mstack.push(5);
     mstack.push(737);
-    //[...]
     mstack.push(0);
     MutantStack<int>::iterator it = mstack.begin();
     MutantStack<int>::iterator ite = mstack.end();
@@ -21,10 +25,42 @@ int main() {
         std::cout << *it << std::endl;
         ++it;
     }
-    std::cout << "------------------------------" << std::endl;
+
+    std::cout << "===========================" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Test2: Creating mutant stack copy" << std::endl;
+    std::cout << std::endl;
+
     MutantStack<int> s(mstack);
     MutantStack<int>::iterator it1;
     for (it1 = s.begin(); it1 != s.end(); ++it1) {
         std::cout << *it1 << std::endl;
+    }
+
+    std::cout << "===========================" << std::endl;
+    std::cout << std::endl;
+    std::cout << "Test3: Comparing mutant stack with std::list" << std::endl;
+    std::cout << std::endl;
+
+    MutantStack<std::string> ms;
+    ms.push("testing");
+    ms.push("mutant");
+    ms.push("stack");
+    ms.push("list"); 
+
+    std::list<std::string> list;
+    list.push_back("testing");
+    list.push_back("mutant");
+    list.push_back("stack");
+    list.push_back("list");
+
+    std::cout << "Mutant stack:" << std::endl;
+    for (MutantStack<std::string>::iterator it1 = ms.begin(); it1 != ms.end(); ++it1) {
+        std::cout << *it1 << std::endl;
+    }
+    std::cout << std::endl;
+    std::cout << "List:" << std::endl;
+    for (std::list<std::string>::iterator itl = list.begin(); itl != list.end(); ++itl) {
+        std::cout << *itl << std::endl;
     }
 }
