6a1f42a3d0673b53cb593f9e75835c7a2d211dfd
pgonzalezpr <pgonzalezpr1@gmail.com>
2024-07-07 18:10:55 +0200

    inception

--- inception/srcs/requirements/mariadb/tools/init.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS mydatabase;
-CREATE USER 'pedrogo'@'%' IDENTIFIED BY 'pedrogopss';
-GRANT ALL PRIVILEGES ON mydatabase.* TO 'pedrogo'@'%' WITH GRANT OPTION;
-FLUSH PRIVILEGES;
-SET PASSWORD FOR 'root'@'localhost' = PASSWORD('newpss');
--- inception/srcs/requirements/nginx/tools/nginx_script.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-if [ ! -f /etc/ssl/pedro-go.42.fr.crt ]; then
-openssl req -newkey rsa:4096 -x509 -days 365 -nodes -out /etc/ssl/pedro-go.42.fr.crt -keyout /etc/ssl/pedro-go.42.fr.key -subj "/CN=pedro-go.42.fr";
-fi
-exec "$@"
